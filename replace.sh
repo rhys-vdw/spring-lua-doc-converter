0f06a04f84a6b86a554317c823abc9fa9c22ef55
Rhys van der Waerden <rhys.vdw@gmail.com>
2024-11-17 15:35:34 +1100

    Add option to give target file name

--- replace.sh
+++ replace.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-cp $1 $1.out
-grep -v "^#" replacements | xargs -I % sed -i -r '%' $1.out
+file="$1"
+if [[ $# -eq 2 ]] ; then
+  cp $1 $2
+  file="$2"
+fi
+
+grep -v "^#" replacements | xargs -I % sed -i -r '%' $file
